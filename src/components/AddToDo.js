73fa872d39ef7658ad3986dcdaa3f77bcec94eff
Audric Skivée <audricskivee@gmail.com>
2021-10-13 16:59:50 +0200

    adding logic here and there in the ToDoList

--- src/components/AddToDo.js
+++ src/components/AddToDo.js
@@ -2,13 +2,15 @@ import React, { useState } from 'react';
 
 function AddToDo (){
     const todos = ["coucou","coucou2"];
-
+    function clickHandler() {
+        // Something...
+      }
     return (
         <div className="addToDo">
             <form>
                 <input  value="" type="text" 
                         placeholder='Type a new ToDo'/>
-                <input type="submit"/>
+                <button>add a ToDo</button>
             </form>
         </div>
 
--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,7 +1,7 @@
 import React,{useState} from "react";
 
 export default function TodoList() {
-    const initialTodos = ["My first todo", "My second todo"];
+    const initialTodos = ["My first todo", "My second todo", "mon gros fiak"];
     const [todos, setTodos] = useState(initialTodos);
     return (
         <div className="toDoList">
