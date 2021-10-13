411d256408683a917ebcaf606b4982e222243f56
Audric Skivée <audricskivee@gmail.com>
2021-10-13 16:14:21 +0200

    adding logic here and there in the ToDoList

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,19 +1,19 @@
-import React, {useState} from "react";
+import React,{useState} from "react";
 
 export default function TodoList() {
     const initialTodos = ["My first todo", "My second todo"];
     const [todos, setTodos] = useState(initialTodos);
-  return (
-      <div className="toDoList">
-      <h1>ToDo's</h1>
+    return (
+        <div className="toDoList">
         <ul>
-            {todos.map((todo) => (
-                <li>
-                <input type="checkbox" /> {todo}
-                </li>
-            ))}
+        {todos.map((todo, index) => (
+            <li key={index}>
+            <input type="checkbox" name={todo} id={todo}/> 
+            <label htmlFor={todo}>{todo}</label> 
+            </li>
+        ))}
         </ul>
-      </div>
-    
-  );
+        </div>
+        
+    );
 }
