48c9bb99a43bed0b8330ed45f126e7a59981d731
Audric Skivée <audricskivee@gmail.com>
2021-10-15 10:45:56 +0200

    design modif

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -17,7 +17,6 @@ export default function Todo({todo, toggleTodo}) {
                     <span></span>
                     <div className='success'>{check}</div>
                     {todo.name}
-                
                 </label> 
                 </li>
         </>
